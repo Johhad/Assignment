40dde0ab2f43a3d3d27f8a53c1c203f2f9f450cf
Johannes H. <johanneshad_95@hotmail.com>
2025-09-17 19:03:44 +0200

    Correction
    updated and corrected "about" page

--- About.py
+++ About.py
@@ -1,39 +1,21 @@
-{
- "cells": [
-  {
-   "cell_type": "markdown",
-   "id": "286528e1",
-   "metadata": {},
-   "source": [
-    "# 🌟 About 🌟 "
-   ]
-  },
-  {
-   "cell_type": "markdown",
-   "id": "02105c6d",
-   "metadata": {},
-   "source": [
-    "Author Johannes Haddad"
-   ]
-  },
-  {
-   "cell_type": "markdown",
-   "id": "30debac7",
-   "metadata": {},
-   "source": [
-    "The purpose of this project was to show the user the probability of risk of stroke between male and female. the user have access to the whole dataset as well as a chart that shows the risk chance between gender. It also provides a final result to show which gender has the higest risk. \n",
-    "\n",
-    "Since I had no access, to due to limited amount of time, to find a real authentic data set I chose to create a\n",
-    "simple synthetc data set that focused on only gender. The results presented are random and not to be used in\n",
-    "a real world setting. Although the data set was synthetic it shows what an app could be used for in a real world setting. This project shows how aggression, simulation and visualization using python and streamlit can do. "
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import streamlit as st
+
+st.set_page_config(
+    page_title=("Johannes Haddad"),
+    page_icon=("👨‍💻"),
+)
+
+st.markdown(
+"""
+
+During my last DSHI course I have gained knowledge on how to use programming as a tool for calculation of data, 
+visualization of data and also how to present data. From using classification, supervised learning to determain
+prediction or classification, grouping up similar data, different machine learning models, their purpse and
+how to use them. Having a background in healthcare I have an understanding of how to apply machine learning 
+into a real world healthcare setting. The possibilites are endelss and the ideas "pops" up at any time. 
+There is still alot to learn and still alot to understand about machine learning. It seems very positiv, although
+machine learning and AI would make healthcare better (which I do believe), I have also learned from DSHI 
+courses that we must not forget about the ethical aspect with machine learning and AI
+
+"""
+)
